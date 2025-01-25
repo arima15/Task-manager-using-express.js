7f93c8afe2d1f10b23b4ee1f7661cd2f45a5e514
Your Name <your.email@example.com>
2025-01-25 20:55:39 +0800

    change to follow middleware guide

--- myapp.js
+++ myapp.js
@@ -3,10 +3,24 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'))
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
+app.get('/about', (req, res) => {
+    res.send('About us')
+});
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`Received data: ${JSON.stringify(data)}`);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
